9df7798c4beaae6202276fc08fc2c7b983e2d2e4
eshoiza <engrjayt200@gmail.com>
2019-11-24 22:05:30 +0100

    project setup

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App__Aside"></div>
+      <div className="App__Form">
+        <div className="PageSwitcher">
+          <a href="#" className="PageSwitcher__Item">
+            {" "}
+            Sign In
+          </a>
+          <a href="#" className="PageSwitcher__Item PageSticher__Item--active">
+            SignUp
+          </a>
+        </div>
+        <div className="FormTitle">
+          <a href="#" className="FormTitle__Link">
+            Sign In
+          </a>{" "}
+          or
+          <a href="#" className="FormTitle__Link FormTitle__Link--Active">
+            Sign Up
+          </a>
+        </div>
+
+        <div className="FormCenter">
+          <form className="FormFields" onSubmit={this.handleSubmit}>
+            <div className="FormFields">
+              <label className="FormFirld__Label" thmlFor="name">
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="FormField__Input"
+                placeholder="Enter your full name"
+                name="name"
+              ></input>
+            </div>
+          </form>
+        </div>
+
+        <div className="FormField">
+          <label className="FormField__Label" htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="FormField__Input"
+            placeholder="Enter your full password"
+            name="password"
+          ></input>
+        </div>
+
+        <div className="FormField">
+          <label className="FormField__Label" htmlFor="email">
+            E-mail Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="FormField__Input"
+            placeholder="Enter your Email address"
+            name="email"
+          ></input>
+        </div>
+      <label className="FormField__CheckboxLabel">
+       <input className="FormField__Checkbox" type="checkbox" name="asAgreed">I agree all statment in</input>
+       <a href="FormField__TermsLink">terms of services</a>
+      </label>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
