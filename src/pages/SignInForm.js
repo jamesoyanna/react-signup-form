579e02334e1868169e042326aae6ede6123c756e
eshoiza <engrjayt200@gmail.com>
2019-11-25 11:30:44 +0100

    added signup form component

--- src/pages/SignInForm.js
+++ src/pages/SignInForm.js
@@ -1,45 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export class SignInForm extends Component {
-constructor(){
+class SignInForm extends Component {
+  constructor() {
     super();
 
     this.state = {
-      email: '',
-      password: '',
-
+      email: "",
+      password: ""
     };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    this.handleChange(e){
+  handleChange(e) {
     let target = e.target;
-    let value = target.type=== 'checkbox' ? target.checked: target.value;
+    let value = target.type === "checkbox" ? target.checked : target.value;
     let name = target.name;
 
-    thi.setState({
-        [name]: value
+    this.setState({
+      [name]: value
     });
+  }
 
-    }
-
-this.handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
-    console.log('The form was sunmitted wuth the following data:')
+
+    console.log("The form was submitted with the following data:");
     console.log(this.state);
-}
+  }
 
+  render() {
+    return (
+      <div className="FormCenter">
+        <form
+          onSubmit={this.handleSubmit}
+          className="FormFields"
+          
+        >
+          <div className="FormField">
+            <label className="FormField__Label" htmlFor="email">
+              E-Mail Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="FormField__Input"
+              placeholder="Enter your email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
 
+          <div className="FormField">
+            <label className="FormField__Label" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="FormField__Input"
+              placeholder="Enter your password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
 
-}
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+          <div className="FormField">
+            <button className="FormField__Button mr-20">Sign In</button>{" "}
+            <Link to="/" className="FormField__Link">
+              Create an account
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignInForm;
--- src/pages/SignUpForm.js
+++ src/pages/SignUpForm.js
@@ -1,71 +1,111 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export class SignUpForm extends Component {
-    render() {
-        return (
-          <div>
-            <div className="FormCenter">
-              <form className="FormFields" onSubmit={this.handleSubmit}>
-                <div className="FormFields">
-                  <label className="FormFirld__Label" htmlFor="name">
-                    Full Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    className="FormField__Input"
-                    placeholder="Enter your full name"
-                    name="name"
-                  ></input>
-                </div>
+class SignUpForm extends Component {
+  constructor() {
+    super();
 
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="password">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="FormField__Input"
-                    placeholder="Enter your full password"
-                    name="password"
-                  ></input>
-                </div>
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+      hasAgreed: false
+    };
 
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="email">
-                    E-mail Address
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="FormField__Input"
-                    placeholder="Enter your Email address"
-                    name="email"
-                  ></input>
-                </div>
-                <label className="FormField__CheckboxLabel">
-                  <input
-                    className="FormField__Checkbox"
-                    type="checkbox"
-                    name="asAgreed"
-                  >
-                    I agree all statment in
-                  </input>
-                  <a href="FormField__TermsLink">terms of services</a>
-                </label>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-                <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign Up</button>
-                  <a href="#" className="FormField__Link">
-                    I'am already member
-                  </a>
-                </div>
-              </form>
-            </div>
+  handleChange(e) {
+    let target = e.target;
+    let value = target.type === "checkbox" ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("The form was submitted with the following data:");
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="FormCenter">
+        <form onSubmit={this.handleSubmit} className="FormFields">
+          <div className="FormField">
+            <label className="FormField__Label" htmlFor="name">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="FormField__Input"
+              placeholder="Enter your full name"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           </div>
-        );
-    }
+          <div className="FormField">
+            <label className="FormField__Label" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="FormField__Input"
+              placeholder="Enter your password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="FormField">
+            <label className="FormField__Label" htmlFor="email">
+              E-Mail Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="FormField__Input"
+              placeholder="Enter your email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="FormField">
+            <label className="FormField__CheckboxLabel">
+              <input
+                className="FormField__Checkbox"
+                type="checkbox"
+                name="hasAgreed"
+                value={this.state.hasAgreed}
+                onChange={this.handleChange}
+              />{" "}
+              I agree all statements in{" "}
+              <a href="" className="FormField__TermsLink">
+                terms of service
+              </a>
+            </label>
+          </div>
+
+          <div className="FormField">
+            <button className="FormField__Button mr-20">Sign Up</button>{" "}
+            <Link to="/sign-in" className="FormField__Link">
+              I'm already member
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignUpForm;
